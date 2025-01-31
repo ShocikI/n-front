cf073f8ccedec5b4671b11d4957d3154a60f1405
ShocikI <szotmicha16@gmail.com>
2025-01-31 16:58:46 +0100

    Searchbar

--- src/app/components/StartSearchbar.tsx
+++ src/app/components/StartSearchbar.tsx
@@ -2,6 +2,8 @@
 import React, { useState, useContext, ChangeEvent } from 'react';
 import { useRouter } from 'next/navigation';
 import { AddressContext } from '../Contexts';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlassLocation, faLocationCrosshairs} from '@fortawesome/free-solid-svg-icons';
 
 const StartSearchbar = () => {
   const [text, setText] = useState('');
@@ -10,18 +12,33 @@ const StartSearchbar = () => {
 
   const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => setText(event.target.value);
 
-  const handleOnClick = () => {
+  
+  const handleGetCurrentLocation = () => {
+    context = text;
+    router.push(`/events?${text}`);
+  };
+
+  const handleGetLocationFromSearchbar = () => {
     context = text;
     router.push(`/events?${text}`);
   };
 
   return (
-    <div className="box-border my-auto">
-      <div className="pxt-80 h-12">
-        <input name="search-input" type="text" placeholder="Gdzie jesteś?" onChange={handleOnChange} />
-        <button className="btn btn-primary px-4 ml-4" type="submit" onClick={handleOnClick}>
-          Szukaj
+    <div className="w-full pl-[10px] bg-white rounded-t-xl">
+      <div className="flex flex-row justify-end pt-1 pxt-80 h-12">
+        
+        <input name="search-input" type="text" placeholder="Where you are?" 
+        className='w-full pr-4 border-none'
+        onChange={handleOnChange} />
+
+        <button className='box-border' type="submit" onClick={handleGetCurrentLocation}>
+          <FontAwesomeIcon className='size-4 mx-2 hover:text-secondary' icon={faLocationCrosshairs} />
         </button>
+
+        <button className='box-border' type="submit" onClick={handleGetLocationFromSearchbar}>
+          <FontAwesomeIcon className='size-6 mx-2 hover:text-secondary' icon={faMagnifyingGlassLocation} />
+        </button>
+
       </div>
     </div>
   );
