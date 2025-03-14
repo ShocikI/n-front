a137b11cd53e1e45b63222cbd99204b3452fd3fb
ShocikI <szotmicha16@gmail.com>
2025-03-14 18:27:18 +0100

    Fix LoggedStartLayout styles

--- src/app/components/layouts/LoggedStartLayout.tsx
+++ src/app/components/layouts/LoggedStartLayout.tsx
@@ -3,9 +3,11 @@ import { Header } from "../Header";
 
 export const LoggedStartLayout = ( ) => {
     return (
-        <div>
+        <div className="flex flex-col h-screen">
             <Header />
-            <GoogleMaps/>
+            <div className="flex h-full items-center justify-evenly">
+                <GoogleMaps/>
+            </div>
         </div>
     )
 }
