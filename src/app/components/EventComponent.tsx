6732005db813c9490152596cc5fcb93b52726ddd
ShocikI <szotmicha16@gmail.com>
2025-04-16 18:54:49 +0200

    Upgrade EventComponent

--- src/app/components/EventComponent.tsx
+++ src/app/components/EventComponent.tsx
@@ -1,33 +1,67 @@
 'use client';
+
 import { FC, useState } from 'react';
-import { Event } from '../data/Event';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import clsx from 'clsx';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pl';
 
-type Props = { event: Event; key: number };
+import { Event } from '../data/interfaces';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+import { client } from '../data/client';
 
-export const EventComponent: FC<Props> = props => {
+dayjs.locale('pl');
+
+type Props = { 
+  event: Event,
+  isOwner: boolean
+};
+
+export const EventComponent: FC<Props> = ({ event, isOwner }) => {
+  const router = useRouter();
   const [isHide, setIsHide] = useState<boolean>(true);
   const handleHiddingDetails = () => setIsHide(!isHide);
   const angleIcon = isHide ? faAngleDown : faAngleUp;
+  var localizedFormat = require("dayjs/plugin/localizedFormat");
+  dayjs.extend(localizedFormat);
 
   return (
-    <li className="flex flex-col box-border m-2 p-1 w-full border-2 border-primary" key={props.event.title}>
+    <div className="flex flex-col box-border m-2 p-4 max-w-[800px] border-2 border-primary">
       <div className="flex flex-row place-content-between px-1.5">
-        <h1 className="font-bold">{props.event.title}</h1>
+        <Label className="font-bold">{event.title}</Label>
         <button className="text-xs" onClick={handleHiddingDetails}>
-          {clsx(isHide && 'Poznaj', !isHide && 'Ukryj', ' szczegóły')}
-          <FontAwesomeIcon icon={angleIcon} className="pl-1" />
+          {clsx(isHide && 'Show', !isHide && 'Hide', ' details')}
+          <FontAwesomeIcon icon={angleIcon} className="pl-2" />
         </button>
       </div>
-      <div className={clsx('grid justify-items-start pl-1.5', !isHide && 'block', isHide && 'hidden')}>
-        <h2>{props.event.date.toDateString()}</h2>
-        <h2>Kategoria: {props.event.category.name}</h2>
-        <h2>{props.event.description}</h2>
-        <h2>Cena: {props.event.price} zł</h2>
-        <h2>Dostępne miejsca: {props.event.avaliable_places}</h2>
+      <div className={clsx('flex flex-row justify-between pl-1.5 mt-4', !isHide && 'block', isHide && 'hidden')}>
+        <div className='flex flex-col gap-2'>
+          <Label>{dayjs(event.date).format('LLL')}</Label>
+          <Label>Kategoria: {event.category.title}</Label>
+          { !!event.price &&
+            <Label>Cena: {event.price} zł</Label>
+            }
+          { !!event.avaliable_places &&
+            <Label>Dostępne miejsca: {event.avaliable_places}</Label>
+            }
+          { !!event.description &&
+            <Label>{event.description}</Label>
+            }
+        </div>
+        <div className='flex flex-col items-end justify-between'>
+          { !!event.image &&
+            <img src={`${process.env.NEXT_PUBLIC_API_URL}/${event.image}`} className='min-w-[160px] h-fit p-4'/>          
+          }
+          { isOwner &&
+            <Button onClick={() => router.push(`/events/${event.id}/`)}>
+              Edit event
+            </Button>
+          }
+        </div>
       </div>
-    </li>
+    </div>
   );
 };
