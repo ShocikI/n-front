4dad1cf1b6fc1685b66b5485f86f0a495ab686e1
ShocikI <szotmicha16@gmail.com>
2025-06-30 15:24:31 +0200

    Add EventSections

---
+++ src/app/components/sections/EventEditableSection.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { Event } from "@/app/data/interfaces"
+import { EditEventForm } from "../forms/EditEventForm"
+import { UpdateEventImageForm } from "../forms/UpdateEventImageForm"
+import { CreateEventLinkForm } from "../forms/CreateEventLinkForm"
+
+type Props = {
+    event: Event,
+    flag: boolean,
+    setFlag: (flag: boolean) => void,
+}
+
+export const EventEditableSection = ({ event, flag, setFlag }: Props) => {
+
+    return (
+    <main className="flex flex-col gap-4">
+        <EditEventForm event={event}/>
+        <section className="flex flex-row gap-4">
+            <CreateEventLinkForm 
+                eventId={event.id}
+                flag={flag}
+                setFlag={setFlag}
+                />
+            <UpdateEventImageForm 
+                event={event} 
+                flag={flag} 
+                setFlag={setFlag}
+                />
+        </section>
+    </main>
+    )
+}
---
+++ src/app/components/sections/EventLinksSection.tsx
@@ -0,0 +1,89 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTiktok, faLinkedin, faXTwitter, faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+
+import { Links } from "@/app/data/interfaces"
+import { useEffect, useState } from "react";
+import { client } from "@/app/data/client";
+import { CreateEventLinkForm } from "../forms/CreateEventLinkForm";
+
+type Props = {
+    id?: number,
+    flag: boolean,
+    setFlag: (flag: boolean) => void,
+    isOwner: boolean,
+    isEditPage?: boolean
+}
+
+const iconMap: Record<string, any> = {
+    linkedin: faLinkedin,
+    tiktok: faTiktok,
+    x: faXTwitter,
+    instagram: faInstagram,
+    facebook: faFacebook,
+    default: faLink
+};
+
+export const EventLinksSection = ({ id, flag, setFlag, isOwner, isEditPage }: Props) => {
+    const [ links, setLinks ] = useState<Links[]>()
+
+    useEffect(() => {
+        const getLinks = async () => {
+            const response = await client.getEventLinks(id);
+            if (response.status == 200) 
+                setLinks(response.data); 
+        };
+
+        if (flag === false) {
+            getLinks();
+        }
+    }, [flag])
+
+    const formatUrl = (url: string) => {
+        return url.startsWith("http://") || url.startsWith("https://")
+            ? url
+            : `https://${url}`
+    };
+
+    const handleDelete = async (linkId: number) => {
+        try {
+            client.deleteEventLink(id, linkId);
+            setFlag(!flag);
+        } catch (error: any) {
+            console.error("Błąd podczas usuwania linku:", error);
+        }
+      };
+
+    const listLinks = () => {
+        return links?.map((link, index) => {
+            const icon = iconMap[link.type.title.toLowerCase()] || iconMap.default;
+
+            return (
+                !!links &&
+                <div key={index} className="flex items-center gap-2 py-2 ">
+                    <FontAwesomeIcon icon={icon} className="text-xl text-gray-600 mr-2 size-8" />
+                    <a href={formatUrl(link.link_url)} target="_blank" rel="noopener noreferrer" 
+                        className="hover:underline"
+                    >
+                        {link.link_url}
+                    </a>
+                    {isOwner && isEditPage &&
+                        <FontAwesomeIcon 
+                            icon={faCircleXmark} 
+                            className="text-xl text-red-600 ml-2 size-6"
+                            onClick={() => handleDelete(link.id)}
+                        />
+                    }
+                </div>
+            )
+        })
+    }
+
+
+    return (
+        <section>
+            { listLinks() }
+        </section>
+    )
+}
