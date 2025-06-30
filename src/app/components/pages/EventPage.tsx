0c58c37f8e4b822eedb0d97f17fbd0d54c515887
ShocikI <szotmicha16@gmail.com>
2025-06-30 15:25:30 +0200

    Rename EventPage -> EventsPage, Add EventPage

--- src/app/components/pages/EventPage.tsx
+++ src/app/components/pages/EventPage.tsx
@@ -1,57 +1,71 @@
 'use client';
-import { useContext, useEffect, useState } from 'react';
-import { useSearchParams, useRouter } from 'next/navigation';
-import { SearchBarContext } from '@/app/Contexts';
-import { client } from '@/app/data/client';
-import { SearchBar } from '../SearchBar';
-import { AllEvents } from '../AllEvents';
-import { Event } from '@/app/data/interfaces';
+
+import { client } from "@/app/data/client";
+import { Event } from "@/app/data/interfaces";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { EditEventForm } from "../forms/EditEventForm";
+import { UnmutableEventSection } from "../sections/UnmutableEventSection";
+import { EventEditableSection } from "../sections/EventEditableSection";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faImage } from "@fortawesome/free-solid-svg-icons";
+import { EventLinksSection } from "../sections/EventLinksSection";
+
+export const EventPage = () => {
+    const router = useRouter();
+    const [ event, setEvent ] = useState<Event>();
+    const [ ownerLogged, setOwnerLogged ] = useState(false)
+    const [ reloadFlag, setReloadFlag ] = useState(false);
+    const pathname = usePathname()
+    const eventId = Number(pathname?.split("/").reverse()[0])
+
+    useEffect(() => {
+        const fetchEvent = async () => {
+            try {
+                const data = await client.getEventById(eventId);
+                console.log(data)
+                const username = data.data.owner.username
+
+                const responseToken = await client.checkToken();
+                if (responseToken === username)
+                    setOwnerLogged(true);
+
+                if (data.status != 200)
+                    router.back()
+
+                setEvent(data.data);
+            } catch (error) {
+                console.error("Event doesn't exists:", error);
+                router.push("/");
+            }
+        };
+
+        fetchEvent();
+        
+    }, [ reloadFlag ]);
+
+    return (
+        <main className="flex flex-col w-2/3 min-w-[350px] gap-4">
+            <UnmutableEventSection event={event} isOwner={ownerLogged}/>
+
+            {event && ownerLogged && 
+            <EventEditableSection 
+                event={event} 
+                flag={reloadFlag} 
+                setFlag={setReloadFlag}
+            />
+            }
+            {event &&
+            <EventLinksSection 
+                id={event.id} 
+                flag={reloadFlag} 
+                setFlag={setReloadFlag}
+                isOwner={ownerLogged}
+                isEditPage={ownerLogged}
+            />
+            }
+        </main>
+    )
 
 
-
-const EventPage = () => {
-  const { query, setQuery } = useContext(SearchBarContext);
-  const [events, setEvents] = useState<Event[]>([]);
-  const searchParams = useSearchParams();
-  const point = searchParams.getAll('point')[0];
-  const radius = searchParams.getAll('radius')[0];
-  const categoryIds = searchParams.getAll('categories').map(categoryId => (categoryId));
-
-  useEffect(() => {
-    const removeSessionStorage = (key: string) => {
-      const old = sessionStorage.getItem(key)
-      if (old) sessionStorage.removeItem(key)
-    }
-    
-    const updateLocation = () => {
-      removeSessionStorage("point");
-      removeSessionStorage("radius");
-
-      sessionStorage.setItem("point", point);
-      sessionStorage.setItem("radius", radius);
-    }
-    updateLocation()
-
-    const fetchEvents = async () => {
-      const updatedPoint = sessionStorage.getItem("point");
-      const updatedRadius = sessionStorage.getItem("radius");
-      
-      const fetchedEvents = await client.getEvents(updatedPoint, updatedRadius, categoryIds);
-      setEvents(fetchedEvents);
-    };
-    fetchEvents();
-  }, []);
-
-  return (
-    <SearchBarContext.Provider value={{ query, setQuery }}>
-      <div className="flex flex-col items-center w-4/5">
-        <div className="p-1.5">
-          <SearchBar />
-        </div>
-        <AllEvents events={events} />
-      </div>
-    </SearchBarContext.Provider>
-  );
-};
-
-export default EventPage;
+}
---
+++ src/app/components/pages/EventsPage.tsx
@@ -0,0 +1,56 @@
+'use client';
+import { useContext, useEffect, useState } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
+import { SearchBarContext } from '@/app/Contexts';
+import { client } from '@/app/data/client';
+import { SearchBar } from '../SearchBar';
+import { AllEvents } from '../AllEvents';
+import { Event } from '@/app/data/interfaces';
+
+
+
+export const EventsPage = () => {
+  const { query, setQuery } = useContext(SearchBarContext);
+  const [events, setEvents] = useState<Event[]>([]);
+  const searchParams = useSearchParams();
+  const point = searchParams.getAll('point')[0];
+  const radius = searchParams.getAll('radius')[0];
+  const categoryIds = searchParams.getAll('categories').map(categoryId => (categoryId));
+
+  useEffect(() => {
+    const removeSessionStorage = (key: string) => {
+      const old = sessionStorage.getItem(key)
+      if (old) sessionStorage.removeItem(key)
+    }
+    
+    const updateLocation = () => {
+      removeSessionStorage("point");
+      removeSessionStorage("radius");
+
+      sessionStorage.setItem("point", point);
+      sessionStorage.setItem("radius", radius);
+    }
+    updateLocation()
+
+    const fetchEvents = async () => {
+      const updatedPoint = sessionStorage.getItem("point");
+      const updatedRadius = sessionStorage.getItem("radius");
+      
+      const fetchedEvents = await client.getEvents(updatedPoint, updatedRadius, categoryIds);
+      setEvents(fetchedEvents);
+    };
+    fetchEvents();
+  }, []);
+
+  return (
+    <SearchBarContext.Provider value={{ query, setQuery }}>
+      <div className="flex flex-col items-center w-4/5">
+        <div className="p-1.5">
+          <SearchBar />
+        </div>
+        <AllEvents events={events} />
+      </div>
+    </SearchBarContext.Provider>
+  );
+};
+
