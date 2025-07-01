6dc925603652f0efaeadcf966e3d97397751c654
ShocikI <szotmicha16@gmail.com>
2025-07-01 12:50:37 +0200

    Simplify SearchBar

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -2,7 +2,7 @@
 import React, { ChangeEvent, useContext, useState } from 'react';
 import { SearchBarContext } from '../Contexts';
 
-const SearchBar = () => {
+export const SearchBar = () => {
   const { query, setQuery } = useContext(SearchBarContext);
   const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
@@ -10,11 +10,9 @@ const SearchBar = () => {
 
   return (
     <div className="box-border my-auto">
-      <div className="pxt-80 h-12">
-        <input type="text" placeholder="" onChange={handleOnChange} value={query} />
+      <div className="h-12">
+        <input type="text" placeholder="Search by name" onChange={handleOnChange} value={query} />
       </div>
     </div>
   );
 };
-
-export default SearchBar;
